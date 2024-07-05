127805155dee5ce209323194fad5e91a07d46f49
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2024-07-05 13:02:50 +0200

    init Ionic

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-button @click="() => router.push('/tabs/')">Login</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+<script setup lang="ts">
+import { IonButton, IonPage, IonHeader, IonToolbar, IonContent, IonTitle} from '@ionic/vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
