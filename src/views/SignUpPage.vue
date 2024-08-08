fa809f3d73e10f696fb481f272486a06709ec970
Diego R. Ammirabile <diegoroberto.ammirabile@gmail.com>
2024-08-08 22:49:20 +0200

    Impl quasi completa di #8

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -6,39 +6,65 @@
       </IonToolbar>
     </IonHeader>
     <IonContent>
-      <IonInput placeholder="Username"></IonInput>
-      <IonInput placeholder="Password"></IonInput>
-      <IonButton onClick={signup}>Sign Up</IonButton>
+      <IonInput placeholder="Username" v-model="username"></IonInput>
+      <IonInput placeholder="Password" v-model="password">
+        <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+      </IonInput>
+      <IonButton @click="singup()">Sign Up</IonButton>
       <p>
         Already have an account? 
         <IonNavLink routerLink="/login">Login</IonNavLink>
       </p>
+      <IonToast
+        :isOpen="singupError"
+        :message="message"
+        position="top"
+        duration="2000"></IonToast>
     </IonContent>
   </IonPage>
 </template>
 
 <script setup lang="ts">
-import { IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonNavLink } from '@ionic/vue';
+import { IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonNavLink, IonInputPasswordToggle, IonToast } from '@ionic/vue';
+import { ref } from 'vue';
+
+const username = ref('angolo180');
+const password = ref('Angolo.180');
+
+const singupError = ref(false);
+const message = ref('');
 
 async function singup() {
+  if (singupError.value) {
+    return; // prevent multiple clicks
+  }
   await fetch('/api/auth', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      username: 'angolo180',
-      password: 'Angolo.180',
+      username: username.value,
+      password: password.value,
     }),
   }).then((response) => {
     if (response.ok) {
       console.log( response.json());
     }
+    if (response.status === 400) {
+      const errorMessage = 'Username already exists. Please try another one.'
+      console.log(errorMessage);
+      message.value = errorMessage;
+      singupError.value = true;
+      setTimeout(() => {
+        singupError.value = false;
+      }, 2000);
+    }
     throw new Error('Network response was not ok.');
   }).catch((error) => {
     console.error('There has been a problem with your fetch operation:', error);
   });
-  //TODO login after signup
+  //TODO login after signup, redirect to login page after successful signup
 }
 
 
